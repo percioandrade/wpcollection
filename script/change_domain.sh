075f99d9c871334c0778561526f065d761275655
Percio Andrade <perciocastelo@gmail.com>
2023-08-03 06:34:38 -0400

    send change_domain and update change_prefix

--- script/change_domain.sh
+++ script/change_domain.sh
@@ -2,7 +2,7 @@
 ##################################################################
 # Author  : Percio Andrade
 # Email   : perciocastelo@gmail.com
-# Info    : Shell script to convert wordpress database prefix to another
+# Info    : Shell script to change wordpress domain to another
 # Version : 1.0
 # changelog :
 # 1.0
@@ -29,7 +29,7 @@ if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
 fi
 
 V='1.1'
-URL='https://raw.githubusercontent.com/percioandrade/wpcollection/main/script/change_prefix.sh'
+URL='https://raw.githubusercontent.com/percioandrade/wpcollection/main/script/change_domain.sh'
 
 # Skip version check
 if [[ " $* " == *" -n "* || " $* " == *" --noversion "* ]]; then
@@ -84,65 +84,51 @@ echo $'
 ------------------------
 '
 
-echo '[!] Checking the current prefix'
+echo '[!] Checking the current domain'
 echo '[!] Trying to establish a connection, please wait....'
 
-# Connecting to MySQL to retrieve the table prefix
 PREFIX=$(mysql -N -s -u "${DB_USER}" -p"${DB_PASS}" -h "${DB_HOST}" -e "SELECT table_name FROM information_schema.tables WHERE table_schema = '${DATABASE}' AND table_name LIKE '%options' LIMIT 1;")
-MYSQL_EXIT_CODE=$?
 PREFIX="${PREFIX%"_options"}"_
+DOMAIN=$(mysql -N -s -u "${DB_USER}" -p"${DB_PASS}" -h "${DB_HOST}" -e "use ${DATABASE}; SELECT * FROM ${PREFIX}options WHERE option_name = 'home';" | awk '{print $3}')
+MYSQL_EXIT_CODE=$?
 
 if [[ $MYSQL_EXIT_CODE -eq 0 ]]; then
-    if [[ -z "${PREFIX}" ]]; then
-        echo '[!] Unable to determine the database '${DATABASE}' prefix'
+    if [[ -z "${DOMAIN}" ]]; then
+        echo '[!] Unable to determine the database '${DATABASE}' domain'
     else
-        echo '[+] The actual database '${DATABASE}' prefix is: '${PREFIX}''
+        echo '[+] The actual database '${DATABASE}' domain is: '${DOMAIN}' with prefix '${PREFIX}' '
     fi
 else
     echo '[!] Connection to MySQL failed. Please check your database credentials'
     exit 1
 fi
 
+# Clean domain https and http
+CLEANED_DOMAIN=${DOMAIN#http://}
+CLEANED_DOMAIN=${DOMAIN#https://}
+
 # Ask if the user wants to skip the database dump
 if [[ " $* " == *" -s "* || " $* " == *" --skip "* ]]; then
     echo '[!] Skip database used, we will not generate a backup'
 else
     # Generate a database dump using the determined prefix
-    DUMP_FILE="${PREFIX}db_backup_$(date +%Y%m%d%H%M%S).sql"
-    mysqldump -u "${DB_USER}" -p"${DB_PASS}" -h "${DB_HOST}" "${DATABASE}" > "${DUMP_FILE}"
+    #DUMP_FILE="${PREFIX}db_backup_$(date +%Y%m%d%H%M%S).sql"
+    #mysqldump -u "${DB_USER}" -p"${DB_PASS}" -h "${DB_HOST}" "${DATABASE}" > "${DUMP_FILE}"
     echo '[!] Dumping database, please wait...'
-    echo '[+] Database dump created on: '$(pwd)/${DUMP_FILE}''
+    #echo '[+] Database dump created on: '$(pwd)/${DUMP_FILE}''
 fi
 
-echo $'[!] Please insert the new prefix. Example: dev'
-echo $'[!] Dont insert long value, the recommended is 2 a 4 characters\n'
+echo $'[!] Please insert the new domain. Example: domain.com.br )\n'
 
-NEW_PREFIX=""
-while IFS= read -r -s -n 1 char; do
-    if [[ $char == $'\0' ]]; then
-        break
-    fi
+read -p 'Insert a domain: ' INPUT_DOMAIN
 
-    # Check if the character is a letter, a digit, or backspace
-    if [[ $char =~ [[:alnum:]] || $char == $'\177' ]]; then
-        if [[ $char == $'\177' ]]; then  # Check for backspace (ASCII value 127)
-            if [[ -n $NEW_PREFIX ]]; then
-                echo -en "\b \b"  # Erase last character
-                NEW_PREFIX=${NEW_PREFIX%?}  # Remove last character from the variable
-            fi
-        else
-            echo -n "$char"
-            NEW_PREFIX+="$char"
-        fi
-    fi
-done
+# Clean input domain https and http
+CLEANED_INPUT_DOMAIN=${INPUT_DOMAIN#http://}
+CLEANED_INPUT_DOMAIN=${INPUT_DOMAIN#https://}
 
-NEW_PREFIX="${NEW_PREFIX}_"
+echo -e "\n[!] This script will change ${CLEANED_DOMAIN} to ${CLEANED_INPUT_DOMAIN}\n"
 
-echo $'\n
-[!] Attention this script will change the '${DATABASE}' actual prefix '${PREFIX}' to new prefix '${NEW_PREFIX}'
-'
-read -p 'Do you want to continue? (y/n): ' RESPONSE
+read -p "Do you want to continue? (y/n): " RESPONSE
 
 # Convert the response to lowercase for comparison
 RESPONSE=$(echo "${RESPONSE}" | tr '[:upper:]' '[:lower:]')
@@ -158,33 +144,18 @@ if [[ "${RESPONSE}" == "y" ]]; then
 
     echo $'\n[!] Continuing...'
 
-    # Change the database prefix
-    if [[ -n "$NEW_PREFIX" ]]; then
-        echo $'[+] Changing database prefix...\n'
-        
-        # Rename all tables with the old prefix to the new prefix
-        for table in $(mysql -u "${DB_USER}" -p"${DB_PASS}" -h "${DB_HOST}" "${DATABASE}" -N -B -e "SHOW TABLES LIKE '${PREFIX}%';"); do
-            new_table="${NEW_PREFIX}${table#${PREFIX}}"
-            mysql -u "${DB_USER}" -p"${DB_PASS}" -h "${DB_HOST}" "${DATABASE}" -e "RENAME TABLE ${DATABASE}.${table} TO ${DATABASE}.${new_table};"
-        done
+    if [[ -n "$CLEANED_INPUT_DOMAIN" ]]; then
+        echo $'[+] Changing database domain...\n'
 
-        echo '[+] Usermeta and options tables updating...'
-
-		# Update usermeta and options tables
         mysql -u "${DB_USER}" -p"${DB_PASS}" -h "${DB_HOST}" "${DATABASE}" -e "
-        UPDATE \`${NEW_PREFIX}options\` SET option_name = '${NEW_PREFIX}user_roles' WHERE option_name = '${PREFIX}user_roles';
-        UPDATE \`${NEW_PREFIX}usermeta\` SET meta_key  = '${NEW_PREFIX}capabilities' WHERE meta_key = '${PREFIX}capabilities';
-        UPDATE \`${NEW_PREFIX}usermeta\` SET meta_key  = '${NEW_PREFIX}user_level' WHERE meta_key = '${PREFIX}user_level';
-        UPDATE \`${NEW_PREFIX}usermeta\` SET meta_key  = '${NEW_PREFIX}autosave_draft_ids' WHERE meta_key = '${PREFIX}autosave_draft_ids';"
-
-        echo $'[+] Displaying values from database\n'
-
-        echo $'Database '${DATABASE}' new prefix is '${NEW_PREFIX}''
+        UPDATE ${PREFIX}options SET option_value = replace(option_value, '${CLEANED_DOMAIN}', '${CLEANED_INPUT_DOMAIN}') WHERE option_name = 'home' OR option_name = 'siteurl';
+        UPDATE ${PREFIX}posts SET guid = replace(guid, '${CLEANED_DOMAIN}','${CLEANED_INPUT_DOMAIN}');
+        UPDATE ${PREFIX}posts SET post_content = replace(post_content, '${CLEANED_DOMAIN}', '${CLEANED_INPUT_DOMAIN}');
+        UPDATE ${PREFIX}postmeta SET meta_value = replace(meta_value,'${CLEANED_DOMAIN}','${CLEANED_INPUT_DOMAIN}');"
 
         echo $'\n[+] All values was updated\n'
-
     else
-        echo $'\n[!] New prefix is empty. No changes made\n'
+        echo $'\n[!] New domain is empty. No changes made\n'
     fi
 
 else
